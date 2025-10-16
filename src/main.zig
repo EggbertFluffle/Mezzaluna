7838d092756b84e308312b344ba6cd2ad6f5e767
Harrison DiAmbrosio <hdiambrosio@gmail.com>
2025-10-16 17:19:03 -0400

    research in ideation.md

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,5 @@ const std = @import("std");
 const Server = @import("server.zig").Server;
 
 pub fn main() !void {
-  std.debug.print("Starting mezzaluna", .{});
   _ = try Server.init();
 }
---
+++ src/output.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+const wl = @import("wayland").server.wl;
+const wlr = @import("wlroots");
+
+pub const Ouput = struct {
+}
